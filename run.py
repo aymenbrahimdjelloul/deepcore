9ec4845cdb2d7ec7f6a2d11cf3dcd83f11f28691
Mr Aymen <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2025-06-14 03:30:24 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+This code or file is pertinent to the 'DeepCore' Project
+Copyright (c) 2023-2025, 'Aymen Brahim Djelloul'. All rights reserved.
+Use of this source code is governed by a MIT license that can be
+found in the LICENSE file.
+
+@author : Aymen Brahim Djelloul
+version : 0.1
+date : 21.05.2025
+license : MIT
+
+"""
+
+# IMPORTS
+from chess.interface import __main__
+
+
+if __name__ == '__main__':
+    # Run it
+    __main__()
